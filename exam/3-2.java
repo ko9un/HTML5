97a5ee01b23db7f5f9b2ebe9a35d1d160575b55a
ko9un <ktw1633@gmail.com>
2022-10-13 13:44:39 +0900

    .

---
+++ exam/3-2.java
@@ -0,0 +1,10 @@
+
+
+int a = 5;
+int b = a++;
+System.out.println(a);
+System.out.println(b);
+System.out.println(++a);
+System.out.println(++b);
+System.out.println(a++ + --b);
+System.out.println(a + b);
